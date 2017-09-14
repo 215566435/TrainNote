d66575c7efc5c7cfea3f3c9a5cedb323bfdc1280
方正 <215566435@qq.com>
2017-09-14 22:33:05 +1200

    [add] login page

--- app/src/Login/views/login.js
+++ app/src/Login/views/login.js
@@ -1,55 +1,65 @@
 import React from 'react'
-import { Form, Icon, Input, Button, Checkbox } from 'antd';
-const FormItem = Form.Item;
+import { Form, Icon, Input, Button} from 'antd';
 
-import './login.less'
+import request from '../../NetWorkUtils/request'
+
+
 import ageUrl from '../../../image/spinner-circle.svg'
 import dumbell from '../../../image/trainNoteIcon.svg'
 
+import './login.less'
+
+
+const Url = 'http://127.0.0.1:3000/api/login'
+const FormItem = Form.Item;
 class Login extends React.Component {
     handleSubmit = (e) => {
         e.preventDefault();
         this.props.form.validateFields((err, values) => {
             if (!err) {
-                console.log('Received values of form: ', values);
+                console.log('Received values of form: ', values)
+                request.Post({
+                    url: Url,
+                    jsonData: values
+                }).then((res)=>{
+                    console.log(res)
+                    console.log(document.cookie)
+                }).catch((error)=>{
+                    console.log(error)
+                })
             }
-        });
+        })
     }
     render() {
         const { getFieldDecorator } = this.props.form;
         return (
             <div style={{ width: '100%', height: '100%', position: 'absolute', display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column' }}  >
                 <div className='iconWrap'>
-                    <img className='dumbell icon' src={ageUrl} style={{ width:200 }} />
+                    <img className='dumbell icon' src={ageUrl} style={{ width: 200 }} />
                 </div>
                 <div className='iconWrap revers'>
-                    <img className='dumbell icon' src={ageUrl} style={{ width:200 }} />
+                    <img className='dumbell icon' src={ageUrl} style={{ width: 200 }} />
                 </div>
                 <div style={{ width: 300 }}>
                     <Form onSubmit={this.handleSubmit} className="login-form">
                         <FormItem>
                             {getFieldDecorator('userName', {
-                                rules: [{ required: true, message: 'Please input your username!' }],
+                                rules: [{ required: true, message: '用户名不能为空!' }],
                             })(
-                                <Input prefix={<Icon type="user" style={{ fontSize: 13 }} />} placeholder="Username" />
+                                <Input prefix={<Icon type="user" style={{ fontSize: 13 }} />} placeholder="邮箱/用户名" />
                                 )}
                         </FormItem>
                         <FormItem>
                             {getFieldDecorator('password', {
-                                rules: [{ required: true, message: 'Please input your Password!' }],
+                                rules: [{ required: true, message: '密码不能为空!' }],
                             })(
-                                <Input prefix={<Icon type="lock" style={{ fontSize: 13 }} />} type="password" placeholder="Password" />
+                                <Input prefix={<Icon type="lock" style={{ fontSize: 13 }} />} type="password" placeholder="密码" />
                                 )}
                         </FormItem>
                         <FormItem>
-                            {getFieldDecorator('remember', {
-                                valuePropName: 'checked',
-                                initialValue: true,
-                            })(
-                                <Button type="primary" htmlType="submit" className="login-form-button">
-                                    Log in
+                            <Button type="primary" htmlType="submit" className="login-form-button">
+                                Log in
                             </Button>
-                                )}
                         </FormItem>
                     </Form>
                 </div>
